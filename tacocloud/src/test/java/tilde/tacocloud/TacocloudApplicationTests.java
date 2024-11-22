1e2dc9b7075e0992e116a32428e38276fa985d71
mrityunjay9693 <mkaykumar123@gmail.com>
2024-11-22 16:56:06 +0530

    project set=up ready

---
+++ tacocloud/src/test/java/tilde/tacocloud/TacocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package tilde.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
