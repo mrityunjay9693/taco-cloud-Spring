f278286b852fb6f1c2f41f8c336ae354ad1e5b4b
mrityunjay9693 <mkaykumar123@gmail.com>
2025-01-19 23:24:06 +0530

    Method declaration in IngredientRepository Interface

---
+++ tacocloud/src/main/java/tilde/tacocloud/data/IngredientRepository.java
@@ -0,0 +1,13 @@
+package tilde.tacocloud.data;
+
+import tilde.tacocloud.Ingredient;
+
+public interface IngredientRepository {
+    // Declaration. 
+    
+    Iterable<Ingredient> findAll();     // It will return a list of ingredients.
+    Ingredient findOne(String id);
+    Ingredient save(Ingredient ingredient);
+
+    //We have to create the body for the above methods somewhere in any class.
+}
