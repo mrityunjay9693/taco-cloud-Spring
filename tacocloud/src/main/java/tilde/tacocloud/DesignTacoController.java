1ee0266cbb157212b4eaf057c8b855a982fb23a2
mrityunjay9693 <mkaykumar123@gmail.com>
2025-01-25 20:35:51 +0530

    updates.

--- tacocloud/src/main/java/tilde/tacocloud/DesignTacoController.java
+++ tacocloud/src/main/java/tilde/tacocloud/DesignTacoController.java
@@ -1,39 +1,42 @@
 package tilde.tacocloud;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+//import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
+
 import tilde.tacocloud.Ingredient.Type;
+import tilde.tacocloud.data.IngredientRepository;
 
 @Slf4j // Use for logging
 @Controller // It will tell that it is a controller
 @RequestMapping("/design") // It will respond to the specified url.
+@SessionAttributes("order")
 public class DesignTacoController {
+    private final IngredientRepository ingredientRepo;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList( // Returns : It returns a list view of the specified array
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-
+        List<Ingredient> ingredients = new ArrayList<>(); // Returns : It returns a list view of the specified array
+        ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+        
         Type[] types = Ingredient.Type.values();
 
         for (Type type : types) {
