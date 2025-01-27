a1c893f9cbb66550ff719611de333a31e8f89ff8
mrityunjay9693 <mkaykumar123@gmail.com>
2025-01-27 18:44:06 +0530

    Taco_Order, Taco_Order_Tacos

--- tacocloud/src/main/resources/schema.sql
+++ tacocloud/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Ingredient (
-    id VARCHAR(4) NOT NULL,
+    id VARCHAR(4) PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
     type VARCHAR(10) NOT NULL
 );
@@ -16,3 +16,22 @@ ALTER TABLE Taco_Ingredients
 ADD FOREIGN KEY (taco) REFERENCES Taco (id);
 ALTER TABLE Taco_Ingredients
 ADD FOREIGN KEY (ingredient) REFERENCES Ingredient (id);
+CREATE TABLE IF NOT EXISTS Taco_Order (
+    id IDENTITY,
+    deliveryName VARCHAR(50) NOT NULL,
+    deliveryStreet VARCHAR(50) NOT NULL,
+    deliveryCity VARCHAR(50) NOT NULL,
+    deliveryState VARCHAR(2) NOT NULL,
+    deliveryZip VARCHAR(10) NOT NULL,
+    ccExpiration VARCHAR(16) NOT NULL,
+    ccCVV VARCHAR(3) NOT NULL,
+    placedAt TIMESTAMP NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Taco_Order_Tacos (
+    tacoOrder BIGINT NOT NULL,
+    taco BIGINT NOT NULL
+);
+ALTER TABLE Taco_Order_Tacos
+ADD FOREIGN KEY (tacoOrder) REFERENCES Taco_Order(id);
+ALTER TABLE Taco_Order_Tacos
+ADD FOREIGN KEY (taco) REFERENCES Taco(id);
